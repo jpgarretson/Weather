d4fb6d42905b6ed1ca84e9d5bc865610c4fd7dfd
Pepper Garretson <jpgarretson@gmail.com>
2013-05-21 18:33:25 -0700

    Initial Commit

---
+++ src/config.h
@@ -0,0 +1,5 @@
+// "us" for Farenheit; "uk", "ca" or "si" for Celsius,
+// "auto" (determined by the user's location)
+#define UNIT_SYSTEM "us"
+#define LATITUDE "32.8928"
+#define LONGITUDE "-83.7062"
